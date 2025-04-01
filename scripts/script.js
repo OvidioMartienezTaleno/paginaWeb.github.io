1740954d7d03a263e84aa60a6601a8e7688b5ca8
ovidiotaleno <141462211+OvidioMartienezTaleno@users.noreply.github.com>
2025-04-01 09:25:57 -0600

    commitErrores
    correccion de errores en la importacion del css al  html

--- scripts/script.js
+++ scripts/script.js
@@ -3,27 +3,27 @@ document.addEventListener('DOMContentLoaded', function() {
     const imagenes = [
         {
             id: 1,
-            src: "assets/diaNacional.jpg",
+            src: "./assets/diaNacional.jpg",
             texto: " Día Nacional de Noruega (17 de mayo): Una celebración patriótica que conmemora la firma de la Constitución Noruega en 1814. Las calles se llenan de desfiles, trajes tradicionales (bunads) y banderas noruegas."
         },
         {
             id: 2,
-            src: "assets/oya.jpg",
+            src: "./assets/oya.jpg",
             texto: "Festival de Música Øya (agosto): Un evento vibrante que reúne a artistas internacionales y locales en un entorno al aire libre. Es uno de los festivales de música más importantes de Noruega."
         },
         {
             id: 3,
-            src: "assets/Spikersuppa.jpg",
+            src: "./assets/Spikersuppa.jpg",
             texto: "Mercado Navideño en Spikersuppa (diciembre):Durante el invierno, Oslo se transforma en un espectáculo de luces y tradiciones nórdicas, con puestos de comida, artesanías y actividades festivas."
         },
         {
             id: 4,
-            src: "assets/musica.jpg",
+            src: "./assets/musica.jpg",
             texto: "Festival Internacional de Música Religiosa de Oslo (marzo): Un evento dedicado a la música religiosa de alta calidad, con conciertos en iglesias y auditorios de la ciudad."
         },
         {
             id: 5,
-            src: "assets/solsticio.jpg",
+            src: "./assets/solsticio.jpg",
             texto: "Solsticio de Verano (junio): Una celebración tradicional que marca el día más largo del año, con fogatas, música y actividades al aire libre en toda Noruega."
         }
     ];
